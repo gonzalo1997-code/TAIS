fe72384eae5eab5864f89cf87bb16b112cb9de6e
GonzaloHer97 <79793119+GonzaloHer97@users.noreply.github.com>
2021-02-28 13:47:46 +0100

    Add files via upload

---
+++ ConjuntosDisjuntos/Prob20_PetrolerosHundidos/Prob20_PetrolerosHundidos/Source.cpp
@@ -0,0 +1,135 @@
+
+// Gonzalo Hernández Hernández
+// TAIS39
+
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include "ConjuntosDisjuntos.h"// propios o los de las estructuras de datos de clase
+
+using namespace std;
+
+using Mapa = vector<string>;
+
+class Petroleo {
+public:
+
+	Petroleo(Mapa const& M, ConjuntosDisjuntos & conjunto) :F(M.size()), C(M[0].size()), visit(F, vector<bool>(C, false)),
+	hayPetroleo(false){
+		for (int i = 0; i < F; i++) {
+			for (int j = 0; j < C; j++) {
+				if (!visit[i][j] && M[i][j] == '#') {
+					unir(M, conjunto, i, j, C);
+				}
+			}
+		}
+	}
+
+	void unir(Mapa const & M, ConjuntosDisjuntos & petroleo, int i, int j, int C) {
+
+		hayPetroleo = true;
+
+		if (correcta(i - 1, j - 1) && M[i - 1][j - 1] == '#')
+			petroleo.unir(i * C + j, (i - 1) * C + j - 1);
+
+		if (correcta(i - 1, j) && M[i - 1][j] == '#')
+			petroleo.unir(i * C + j, (i - 1) * C + j);
+
+		if (correcta(i - 1, j + 1) && M[i - 1][j + 1] == '#')
+			petroleo.unir(i * C + j, (i - 1) * C + j + 1);
+
+		if (correcta(i, j - 1) && M[i][j - 1] == '#')
+			petroleo.unir(i * C + j, i * C + j - 1);
+
+		if (correcta(i, j + 1) && M[i][j + 1] == '#')
+			petroleo.unir(i * C + j, i * C + j + 1);
+
+		if (correcta(i + 1, j - 1) && M[i + 1][j - 1] == '#')
+			petroleo.unir(i * C + j, (i + 1) * C + j - 1);
+
+		if (correcta(i + 1, j) && M[i + 1][j] == '#')
+			petroleo.unir(i * C + j, (i + 1) * C + j);
+
+		if (correcta(i + 1, j + 1) && M[i + 1][j + 1] == '#')
+			petroleo.unir(i * C + j, (i + 1) * C + j + 1);
+
+	}
+
+	int maximoConjunto(ConjuntosDisjuntos & conjunto) const {
+
+		if (hayPetroleo)
+			return max(conjunto.maxConjunto(), 1);
+		return 0;
+	}
+
+private:
+	int F, C;
+	vector<vector<bool>> visit;
+	bool hayPetroleo;
+	bool correcta(int i, int j)const {
+		return 0 <= i && i < F && 0 <= j && j < C;
+	}
+
+};
+
+// resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+
+	// leer los datos de la entrada
+	int F, C;
+	cin >> F >> C;
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	cin.ignore();
+
+	Mapa mapa(F);
+	ConjuntosDisjuntos conjunto(F*C);
+
+	for (string & linea : mapa)
+		getline(cin, linea);
+
+
+	Petroleo petroleo(mapa, conjunto);
+	cout <<  petroleo.maximoConjunto(conjunto) <<" ";
+
+	long int nuevo;
+	cin >> nuevo;
+
+	for (long int i = 0; i < nuevo; i++) {
+		int a, b;
+		cin >> a >> b;
+		mapa[a-1][b-1] = '#';
+		petroleo.unir(mapa, conjunto, a - 1, b - 1, C);
+		cout << petroleo.maximoConjunto(conjunto) << " ";
+	}
+
+
+	cout << "\n";
+
+	return true;
+}
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+	return 0;
+}
---
+++ Grafos dirigidos/Prob17_OrdenandoTareas/Prob17_OrdenandoTareas/Source.cpp
@@ -0,0 +1,100 @@
+
+// Gonzalo Hernández Hernández
+// TAIS39
+
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <queue>
+#include "Digrafo.h" // propios o los de las estructuras de datos de clase
+
+using namespace std;
+
+
+class Tarea {
+public:
+	Tarea(Digrafo const& g) :visit(g.V(), false), apilado(g.V(), false), _orden(),hayciclo(false) {
+		for (int v = 0; v <g.V(); v++) {
+			if (!visit[v])
+				dfs(g, v);
+		}
+	}
+	bool hayCiclo() const { return hayciclo; }
+	deque<int> const& orden() const {
+		return _orden;
+	}
+private:
+	vector<bool> visit;
+	vector<bool> apilado;
+	deque<int> _orden;
+	bool hayciclo;
+
+	void dfs(Digrafo const& g, int v) {
+		apilado[v] = true;
+		visit[v] = true;
+		for (int w : g.ady(v)) {
+			if (apilado[w]) {
+				hayciclo = true;
+				return;
+			}
+			if (!visit[w]) {
+				dfs(g,w);
+			}
+		}
+		_orden.push_back(v+1);
+		apilado[v] = false;
+	}
+
+};
+
+// resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+
+	// leer los datos de la entrada
+	long int V, A;
+	cin >> V >> A;
+	if (!std::cin)  // fin de la entrada
+		return false;
+	Digrafo g(V);
+
+	for (int i = 0; i < A; i++) {
+		long int a, b;
+		cin >> a >> b;
+		g.ponArista(a - 1, b - 1);
+	}
+	Tarea tarea(g.inverso());
+	// escribir sol
+	if (tarea.hayCiclo()) {
+		cout << "Imposible";
+	}
+	else
+	{
+		for (int i = 0; i < tarea.orden().size(); i++) {
+			cout << tarea.orden()[i] << " ";
+		}
+	}
+	cout << "\n";
+	return true;
+}
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+	return 0;
+}
